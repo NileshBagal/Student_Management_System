0e7a66959973b7323a3386026563f728b1099707
Nilesh <n2bagal@gmail.com>
2019-09-10 23:53:42 +0530

    first commit

---
+++ StudentApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    #sid = request.GET["id"]
+   # student = Student.objects.get(1)
+   # print(student)
+   # context = {
+    #    'student': student
+    #}      , context
+    return render(request,'studentHome.html')
+def attend(request):
+    return render(request,'studentAttend.html')
